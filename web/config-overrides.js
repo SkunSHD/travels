23181873ab130f32ca1de9b6f1357bcd50f77bc5
Vladimir <andiwillfly@gmail.com>
2019-06-12 14:35:33 +0300

    Add fxies for web

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -4,5 +4,8 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 module.exports = function override(config, env) {
 	config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
 
+	// https://github.com/necolas/react-native-web/blob/master/docs/guides/getting-started.md
+	config.resolve.alias = { 'react-native$': 'react-native-web' };
+
 	return config;
 };
--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.box}>
+				<Text style={styles.text}>Hello, world!</Text>
+			</View>
+		);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const styles = StyleSheet.create({
+	box: { padding: 10 },
+	text: { fontWeight: 'bold' }
+});
+
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
